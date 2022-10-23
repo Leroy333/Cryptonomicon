2347a6105d1f59af9697fffc59bad23a369e2b01
Leroy333 <stud0000225651@study.utmn.ru>
2022-10-23 15:29:43 +0500

    remove app.css

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,7 @@
                 placeholder="Например DOGE"
               />
             </div>
-            <div
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
-            >
+            <div class="flex bg-white p-1 rounded-md shadow-md flex-wrap">
               <span
                 @click="ticker = 'BTC'"
                 class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
@@ -219,5 +217,3 @@ export default {
   },
 };
 </script>
-
-<style src="./app.css"></style>
